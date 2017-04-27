3b00881c9b78804ec1aaecda0dd9706fb082a9f5
Emanuele <ema.caruso2@gmail.com>
2017-04-27 18:03:42 +0200

    still not work

--- index.js
+++ index.js
@@ -12,23 +12,33 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if(err){
-        console.log('Error connecting to DB: '+err.message);
+    if(err) {
+        console.log('Error connecting to DB: ' + err.message);
         return;
     }
     console.log('Connected');
 
 
+
+    //console.log('the first line of the table is:\n'+query('SELECT * FROM cats WHERE id = 1;'));
+
 });
+
+function query(req, callback) {
+    connection.query(req, function (err, results, fields) {
+        if (typeof(callback) === "function")
+            callback(results);
+    });
+}
+
+query('SELECT * FROM cats', function(err,results, fields){
+    if(!err)
+        console.log('The data are:', results);
+    else
+        console.log('Error in query: ', err.message);
+});
+
 
 connection.end(function (err) {
-    console.log('Disconnected from the DB')
-});
-
-connection.query('SELECT * FROM cats WHERE id = \'1\';', function (err, data) {
-    if (err) {
-     throw err;
-    }
-
-    console.log('Data recived\n'+data);
+    console.log('Disconnected from the DB');
 });
